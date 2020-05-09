dbf21f3ba043e64050e1acaed78e3c58726f564c
Sarthak Koherwal <sarthakkoherwal@gmail.com>
2020-05-09 21:15:53 +0530

    Made some changes to Employee Class - Added functions to call 					      objects of other classes 					      such as Union,hourlySalary,monthly                                              Salaries,monthlyCommissionedSalary                                              to initialize their private 					      variables. 	Created class Union which contains employee who are apart of 	employee union . 	Created class hourlySalary which contains employee who are given 	salary on the basis of hours worked 	Created class monthlySalaried which contains employee who are 	given salary at the end of each month. 	Created class monthlyCommissionedSalary which contains employee 	who are given monthly salary as well as bonuses based on their 	sales record.
    	Completed the Add new employee functionality.

--- SourceCode/Code.java
+++ SourceCode/Code.java
@@ -1,27 +1,80 @@
 package FilesHere;
 import java.util.Scanner;
+import java.sql.*;
 class Employee{
 	private static int count;
+	boolean Umember;
 	private int Id;
 	private String Name,Address;
-	Employee(){
+	Employee(String Name,String Address,boolean Umember){
 		count++;
 		Id=count;
-	}
-	public void setName(String Name){
 		this.Name=Name;
-	}
-	public void setAddress(String Address){
 		this.Address=Address;
+		this.Umember=Umember;
 	}
 	public String getName(){
 		return this.Name;
 	}
 	public String getAddress(){
-		return Address;
+		return this.Address;
 	}
 	public int getId(){
-		return Id;
+		return this.Id;
+	}
+	public void setUnionInfo(int Id,int memberFees,int serviceCharge){
+		Union u=new Union(memberFees,serviceCharge,Id);
+	}
+	public void setHourlyInfo(int Id){
+		hourlySalary h=new hourlySalary(Id);
+	}
+	public void setMonthlyInfo(int Id,int salary){
+		monthlySalaried m=new monthlySalaried(Id,salary);
+	}
+	public void setMonthySalesInfo(int Id,int salary){
+		monthlyCommissionedSalary ms=new monthlyCommissionedSalary(Id,salary);
+	}
+}
+class Union{
+	private int memberFees,serviceCharge,Id;
+	Union(int memberFees,int serviceCharge,int Id){
+		this.memberFees=memberFees;
+		this.serviceCharge=serviceCharge;
+		this.Id=Id;
+	}
+	public int getMemberFees(){
+		return this.memberFees;
+	}
+	public int getServiceCharge(){
+		return this.serviceCharge;
+	}
+}
+class hourlySalary{
+	private static int rate;
+	private int Id,hrs,date;
+	static{
+		rate=10;
+	}
+	hourlySalary(int Id){
+		this.Id=Id;
+	}
+}
+class monthlySalaried{
+	private int salary,Id;
+	monthlySalaried(int Id,int salary){
+		this.Id=Id;
+		this.salary=salary;
+	}
+}
+class monthlyCommissionedSalary{
+	private static double commissionrate;
+	private int date,amountsale,salary,Id;
+	static{
+		commissionrate=5.5;
+	}
+	monthlyCommissionedSalary(int Id,int salary){
+		this.Id=Id;
+		this.salary=salary;
 	}
 }
 public class Code{
@@ -31,16 +84,52 @@ public class Code{
 		int choice=in.nextInt();
 		if(choice==1){
 			String Name,Address;
-			int Id;
+			int Id,paymentRecieveMethod;
+			boolean Umember;
 			while(choice==1){
-				Employee E=new Employee();
 				in.nextLine();
 				System.out.println("Enter Name of Employee");
 				Name=in.nextLine();
 				System.out.println("Enter Address of Employee");
 				Address=in.nextLine();
-				System.out.println("Press 1 to add another new Employee or 0 to exit");
+				System.out.println("Enter the method in which the Employee recieves his salary");
+				System.out.println("Press 1 to mail paycheck to postal address");
+				System.out.println("Press 2 to recieve paycheck from paymaster");
+				System.out.println("Press 3 for bank transfer");
+				paymentRecieveMethod=in.nextInt();
+				System.out.println("Press 1 if the employee is a member of Employee Union ans 0 if he is not a member");
+				Umember=(1==in.nextInt());
+				Employee E=new Employee(Name,Address,Umember);
 				Id=E.getId();
+				if(Umember==true){
+					int memberFees,serviceCharge;
+					System.out.println("Enter membership fees");
+					memberFees=in.nextInt();
+					System.out.println("Enter Service Charge");
+					serviceCharge=in.nextInt();
+					E.setUnionInfo(Id,memberFees,serviceCharge);
+				}
+				System.out.println("Enter the payment method for Employee");
+				System.out.println("Press 1 for hourly payment");
+				System.out.println("Press 2 for monthly payment");
+				System.out.println("Press 3 for monthly payment with sales incentive");
+				int paymentMethod=in.nextInt();
+				if(paymentMethod==1){
+					E.setHourlyInfo(Id);
+				}
+				else if(paymentMethod==2){
+					int salary;
+					System.out.println("Enter monthly salary");
+					salary=in.nextInt();
+					E.setMonthlyInfo(Id,salary);
+				}
+				else if(paymentMethod==3){
+					int salary;
+					System.out.println("Enter monthly Salary");
+					salary=in.nextInt();
+					E.setMonthySalesInfo(Id,salary);
+				}
+				System.out.println("Press 1 to add another new Employee or 0 to exit");
 				choice=in.nextInt();
 			}
 		}
