75a3d7ab95f01729992bad83e192ca4e788e0d3b
Sarthak Koherwal <sarthakkoherwal@gmail.com>
2020-05-09 17:33:29 +0530

    Created a Class Employee which contains three variables 	Name,Address and Id 	Used a static variable count to count the number of objects of 	Employee type created which serves as the Employee ID 	Employee class also contains get() and set() methods for Name 	and Address

--- SourceCode/Code.java
+++ SourceCode/Code.java
@@ -1,20 +1,46 @@
 package FilesHere;
 import java.util.Scanner;
+class Employee{
+	private static int count;
+	private int Id;
+	private String Name,Address;
+	Employee(){
+		count++;
+		Id=count;
+	}
+	public void setName(String Name){
+		this.Name=Name;
+	}
+	public void setAddress(String Address){
+		this.Address=Address;
+	}
+	public String getName(){
+		return this.Name;
+	}
+	public String getAddress(){
+		return Address;
+	}
+	public int getId(){
+		return Id;
+	}
+}
 public class Code{
 	public static void main(String[] args){
 		Scanner in=new Scanner(System.in);
 		System.out.println("Press 1 to add new Employee");
 		int choice=in.nextInt();
-		// int choice=1;
 		if(choice==1){
 			String Name,Address;
+			int Id;
 			while(choice==1){
+				Employee E=new Employee();
 				in.nextLine();
 				System.out.println("Enter Name of Employee");
 				Name=in.nextLine();
 				System.out.println("Enter Address of Employee");
 				Address=in.nextLine();
 				System.out.println("Press 1 to add another new Employee or 0 to exit");
+				Id=E.getId();
 				choice=in.nextInt();
 			}
 		}
