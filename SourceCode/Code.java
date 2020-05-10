522168e2e65a60d49b2b1c13c602e1c95334b68b
Sarthak Koherwal <sarthakkoherwal@gmail.com>
2020-05-10 16:16:34 +0530

    Completed Functionality to add new Employee to database 	Assumed database has five tables 	employee(Id,Name,Address,paymentRecieveMethod,paymentMethod,lastpaid) 	Union(Id,servicecharge,date) 	Hourly(Id,rate,hrs,Date) 	monthly(Id,salary) 	monthlySales(Id,salary,amountsales,date,commissionrate)
    	Default Commission rate is 5.5% until changes by user
    	Default rate for payment to hourly paid employees is 10%
    
    	Whenever a new employee joind his lastpaid date is joining date
    	and rest details are taken from user.
    
    	If he is a member of union a record is added in Union table.
    
    	Based on paymentMethod a record is added in hourly,monthly or
    	monthlySales table.
    
    	Every employee is added  to the employee table.

--- SourceCode/Code.java
+++ SourceCode/Code.java
@@ -1,110 +1,103 @@
 package FilesHere;
 import java.util.Scanner;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Random;
+import java.util.Calendar;
 import java.sql.*;
-class Employee{
-	private static int count;
-	boolean Umember;
-	private int Id,paymentMethod,paymentRecieveMethod;
-	private String Name,Address;
-	Employee(){
-		Id=0;
-		paymentMethod=0;
-		paymentRecieveMethod=0;
-		Name="";
-		Address="";
-		Umember=false;
-	}
-	Employee(String Name,String Address,boolean Umember,int paymentRecieveMethod){
-		count++;
-		Id=count;
-		this.Name=Name;
-		this.Address=Address;
-		this.Umember=Umember;
-		this.paymentRecieveMethod=paymentRecieveMethod;
-	}
-	public void setPaymentMethod(int paymentMethod){
-		this.paymentMethod=paymentMethod;
-	}
-	public String getName(){
-		return this.Name;
-	}
-	public String getAddress(){
-		return this.Address;
-	}
-	public int getId(){
-		return this.Id;
-	}
-	public void setUnionInfo(int Id,int memberFees,int serviceCharge){
-		Union u=new Union(memberFees,serviceCharge,Id);
-	}
-	public void setHourlyInfo(int Id){
-		hourlySalary h=new hourlySalary(Id);
-	}
-	public void setMonthlyInfo(int Id,int salary){
-		monthlySalaried m=new monthlySalaried(Id,salary);
-	}
-	public void setMonthySalesInfo(int Id,int salary){
-		monthlyCommissionedSalary ms=new monthlyCommissionedSalary(Id,salary);
-	}
-	public void timeCard(){
-		hourlySalary temp=new hourlySalary();
-		temp.insertTimeCard();
-	}
-	public void receiptSales(){
-		monthlyCommissionedSalary temp=new monthlyCommissionedSalary();
-		temp.insertReceiptSales();
-	}
-	public void unionFees(){
-		Union temp=new Union();
-		temp.updateUnionFees();
-	}
-	public void delete(int id){
-		//fetch the record form database whose Id matches with the Employee Id to be deleted
-		if(Umember){
-			Union temp=new Union();
-			temp.deleteMember(id);
-		}
-		if(paymentMethod==1){
-			hourlySalary temp=new hourlySalary();
-			temp.deleteMember(id);
-		}
-		else if(paymentMethod==2){
-			monthlySalaried temp=new monthlySalaried();
-			temp.deleteMember(id);
-		}
-		else{
-		monthlyCommissionedSalary temp=new monthlyCommissionedSalary();
-		temp.deleteMember(id);
-		}
-		//delete record from Employee table
+class Connect{
+	private static String url="jdbc:sqlserver//localhost : 6675; database=Payroll";
+	private static String name="sa";
+	private static String pass="sarthak";
+	public static Connection obj() throws Exception{
+		class.forname("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		Connection conn=DriverManager.getConnection(url,name,pass);
+		return conn;
 	}
 }
+class Employee{
+	private static int count;
+	Employee(){
+		count++;
+	}
+	public void insertRecord(int Id,String Name,String Address,int Umember,int paymentRecieveMethod,int paymentMethod,String lastpaid){
+		Connection conn=Connect.obj();
+		Statement st=conn.createStatement();
+		String q="insert into employee values("+Id+","+Name+","+Address+","+Umember+","+paymentRecieveMethod+","+paymentMethod+lastpaid+")";
+		st.executeUpdate(q);
+		conn.close();
+	}
+	public int getId(){
+		return count;
+	}
+	public void setUnionInfo(int Id){
+		Union u=new Union();
+		u.insertRecord(Id);
+	}
+	public void setHourlyInfo(int Id){
+		hourlySalary h=new hourlySalary();
+		h.insertHourlyInfo(Id);
+	}
+	public void setMonthlyInfo(int Id,int salary){
+		monthlySalaried m=new monthlySalaried();
+		m.insertMonthlyInfo(Id,salary);
+	}
+	public void setMonthySalesInfo(int Id,int salary){
+		monthlyCommissionedSalary ms=new monthlyCommissionedSalary();
+		ms.setMonthlySalesInfo(Id,salary);
+	}
+	// public void timeCard(){
+	// 	hourlySalary temp=new hourlySalary();
+	// 	temp.insertTimeCard();
+	// }
+	// public void receiptSales(){
+	// 	monthlyCommissionedSalary temp=new monthlyCommissionedSalary();
+	// 	temp.insertReceiptSales();
+	// }
+	// public void unionFees(){
+	// 	Union temp=new Union();
+	// 	temp.updateUnionFees();
+	// }
+	// public void delete(int id){
+	// 	//fetch the record form database whose Id matches with the Employee Id to be deleted
+	// 	if(Umember){
+	// 		Union temp=new Union();
+	// 		temp.deleteMember(id);
+	// 	}
+	// 	if(paymentMethod==1){
+	// 		hourlySalary temp=new hourlySalary();
+	// 		temp.deleteMember(id);
+	// 	}
+	// 	else if(paymentMethod==2){
+	// 		monthlySalaried temp=new monthlySalaried();
+	// 		temp.deleteMember(id);
+	// 	}
+	// 	else{
+	// 	monthlyCommissionedSalary temp=new monthlyCommissionedSalary();
+	// 	temp.deleteMember(id);
+	// 	}
+	// 	//delete record from Employee table
+	// }
+}
 class Union{
-	private int memberFees,serviceCharge,Id;
-	Union(){
-		Id=0;
-		memberFees=0;
-		serviceCharge=0;
+	private int serviceCharge,Id;
+	String date;
+	public void insertRecord(int Id){
+		Connection conn=Connect.obj();
+		Statement st=conn.createStatement();
+		String q="insert into employee values("+Id+")";
+		st.executeUpdate(q);
+		conn.close();
 	}
-	Union(int memberFees,int serviceCharge,int Id){
-		this.memberFees=memberFees;
-		this.serviceCharge=serviceCharge;
-		this.Id=Id;
-	}
-	public int getMemberFees(){
-		return this.memberFees;
-	}
-	public int getServiceCharge(){
-		return this.serviceCharge;
-	}
-	public void updateUnionFees(){
-		//for loop which iterates over all values in table Union and for every person in the table updates their membershipFees and serviceCharge
-		System.out.println("Enter membership fees");
-		System.out.println("Enter service charges");
-	}
-	public void deleteMember(int id){
-		//delete member from table Union whose ID matches with id
-	}
+	// public void updateUnionFees(){
+	// 	//for loop which iterates over all values in table Union and for every person in the table updates their membershipFees and serviceCharge
+	// 	System.out.println("Enter membership fees");
+
+	// 	System.out.println("Enter service charges");
+	// }
+	// public void deleteMember(int id){
+	// 	//delete member from table Union whose ID matches with id
+	// }
 }
 class hourlySalary{
 	private static int rate;
@@ -112,36 +105,34 @@ class hourlySalary{
 	static{
 		rate=10;
 	}
-	hourlySalary(){
-		Id=0;
-		hrs=0;
-		date=0;
+	public void insertHourlyInfo(int Id){
+		Connection conn=Connect.obj();
+		Statement st=conn.createStatement();
+		String q="insert into Hourly(Id) values("+Id+")";
+		st.executeUpdate(q);
+		conn.close();
 	}
-	hourlySalary(int Id){
-		this.Id=Id;
-	}
-	public void insertTimeCard(){
-		//for loop which iterates over all values in table hourlySalary and for every person in the table inserts their working hours for the current day
-		System.out.println("Enter Date");
-		System.out.println("Enter working hrs");
-	}
-	public void deleteMember(int id){
-		//delete member from table hourly whose ID matches with id
-	}
+	// public void insertTimeCard(){
+	// 	//for loop which iterates over all values in table hourlySalary and for every person in the table inserts their working hours for the current day
+	// 	System.out.println("Enter Date");
+	// 	System.out.println("Enter working hrs");
+	// }
+	// public void deleteMember(int id){
+	// 	//delete member from table hourly whose ID matches with id
+	// }
 }
 class monthlySalaried{
 	private int salary,Id;
-	monthlySalaried(){
-		salary=0;
-		Id=0;
+	public void insertMonthlyInfo(int Id,int salary){
+		Connection conn=Connect.obj();
+		Statement st=conn.createStatement();
+		String q="insert into Hourly(Id,salary) values("+Id+","+salary+")";
+		st.executeUpdate(q);
+		conn.close();
 	}
-	monthlySalaried(int Id,int salary){
-		this.Id=Id;
-		this.salary=salary;
-	}
-	public void deleteMember(int id){
-		//delete member from table monthlySalaried whose ID matches with id
-	}
+	// public void deleteMember(int id){
+	// 	//delete member from table monthlySalaried whose ID matches with id
+	// }
 }
 class monthlyCommissionedSalary{
 	private static double commissionrate;
@@ -149,35 +140,33 @@ class monthlyCommissionedSalary{
 	static{
 		commissionrate=5.5;
 	}
-	monthlyCommissionedSalary(){
-		commissionrate=0.0;
-		date=0;
-		amountsale=0;
-		salary=0;
-		Id=0;
+	public void setMonthlySalesInfo(int Id,int salary){
+		Connection conn=Connect.obj();
+		Statement st=conn.createStatement();
+		String q="insert into Hourly(Id,salary,) values("+Id+","+salary+")";
+		st.executeUpdate(q);
+		conn.close();
 	}
-	monthlyCommissionedSalary(int Id,int salary){
-		this.Id=Id;
-		this.salary=salary;
-	}
-	public void insertReceiptSales(){
-		//for loop which iterates over all values in table monthlyCommissionedSalary and for every person in the table inserts their sales amount and date
-		System.out.println("Enter Date");
-		System.out.println("Enter sales amount");
-	}
-	public void deleteMember(int id){
-		//delete member from table monthlyCommisionedSalary whose ID matches with id
-	}
+	// public void insertReceiptSales(){
+	// 	//for loop which iterates over all values in table monthlyCommissionedSalary and for every person in the table inserts their sales amount and date
+	// 	System.out.println("Enter Date");
+	// 	System.out.println("Enter sales amount");
+	// }
+	// public void deleteMember(int id){
+	// 	//delete member from table monthlyCommisionedSalary whose ID matches with id
+	// }
 }
 public class Code{
 	public static void main(String[] args){
+		String  lastpaid = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
 		Scanner in=new Scanner(System.in);
 		System.out.println("Press 1 to add new Employee");
 		int choice=in.nextInt();
 		if(choice==1){
 			String Name,Address;
 			int Id,paymentRecieveMethod;
-			boolean Umember;
+			int Umember;
+			Employee E=new Employee();
 			while(choice==1){
 				in.nextLine();
 				System.out.println("Enter Name of Employee");
@@ -190,16 +179,10 @@ public class Code{
 				System.out.println("Press 3 for bank transfer");
 				paymentRecieveMethod=in.nextInt();
 				System.out.println("Press 1 if the employee is a member of Employee Union ans 0 if he is not a member");
-				Umember=(1==in.nextInt());
-				Employee E=new Employee(Name,Address,Umember,paymentRecieveMethod);
+				Umember=in.nextInt();
 				Id=E.getId();
-				if(Umember==true){
-					int memberFees,serviceCharge;
-					System.out.println("Enter membership fees");
-					memberFees=in.nextInt();
-					System.out.println("Enter Service Charge");
-					serviceCharge=in.nextInt();
-					E.setUnionInfo(Id,memberFees,serviceCharge);
+				if(Umember==1){
+					E.setUnionInfo(Id);
 				}
 				System.out.println("Enter the payment method for Employee");
 				System.out.println("Press 1 for hourly payment");
@@ -221,9 +204,9 @@ public class Code{
 					salary=in.nextInt();
 					E.setMonthySalesInfo(Id,salary);
 				}
-				E.setPaymentMethod(paymentMethod);
 				System.out.println("Press 1 to add another new Employee or 0 to exit");
 				choice=in.nextInt();
+				E.insertRecord(Id,Name,Address,Umember,paymentRecieveMethod,paymentMethod,lastpaid);
 			}
 		}
 		System.out.println("Press 2 if you want to delete an employee");
@@ -234,16 +217,16 @@ public class Code{
 				System.out.println("Enter the employee ID to be deleted");
 				Id=in.nextInt();
 				Employee temp=new Employee();
-				temp.delete(Id);
+				// temp.delete(Id);
 				System.out.println("Press 2 to delete another record or 0 to exit");
 				choice=in.nextInt();
 			}
 		}
-		Employee e=new Employee();
-		e.timeCard();
+		// Employee e=new Employee();
+		// e.timeCard();
 		//fetch today's date and it if is sunday then update sales receipt for every employee in table monthlyCommissionedSalary
-		e.receiptSales();
+		// e.receiptSales();
 		//fetch today's fate and if it is sunday then update Union information for all employees who belong to the union
-		e.unionFees();
+		// e.unionFees();
 	}
 }
